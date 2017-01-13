09474b5dac1d763ac0577e06b5404322a0e36d8a
Emile <emile.app@gmail.com>
2017-01-13 17:41:29 +0200

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Player : MonoBehaviour {
+
+	public float walkSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float inputH = Input.GetAxisRaw("Horizontal");
+		float inputV = Input.GetAxisRaw("Vertical");
+
+		Debug.Log("inputH: " + inputH);
+		Debug.Log("inputV: " + inputV);
+
+		Vector3 velocityDirection = new Vector3(inputH, inputV, 0);
+
+		transform.Translate(velocityDirection * walkSpeed * Time.deltaTime);
+
+	}
+}
