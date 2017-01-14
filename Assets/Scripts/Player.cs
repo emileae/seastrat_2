f78f0cb767539957ac2e5fea4ae693aa8fe46e60
Emile <emile.app@gmail.com>
2017-01-14 19:05:53 +0200

    added gravity to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,47 @@ using System;
 
 public class Player : MonoBehaviour {
 
+	private float gravitySpeed = -3.0f;
 	public float walkSpeed = 2.0f;
+	private bool grounded = false;
+	public LayerMask groundLayer;
+
+	// ground check
+	public Transform isGrounded;
+	private float groundCheckRadius = 0.2f;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
-		float inputH = Input.GetAxisRaw("Horizontal");
-		float inputV = Input.GetAxisRaw("Vertical");
+		float inputH = Input.GetAxisRaw ("Horizontal");
+		float inputV = Input.GetAxisRaw ("Vertical");
 
-		Debug.Log("inputH: " + inputH);
-		Debug.Log("inputV: " + inputV);
+		float xMove = inputH;
+		float zMove = inputV;
 
-		Vector3 velocityDirection = new Vector3(inputH, inputV, 0);
+		// gravity
+		grounded = Physics.CheckSphere (isGrounded.position, groundCheckRadius, groundLayer);
+		if (!grounded) {
+			Debug.Log ("Should not be falling");
+			zMove += gravitySpeed;
+		} else {
+			zMove = 0f;
+		}
+
+		Vector3 velocityDirection = new Vector3 (xMove, 0, zMove);
 
 		transform.Translate(velocityDirection * walkSpeed * Time.deltaTime);
+
+	}
+
+	void FixedUpdate(){
 
 	}
 }
