6babf93c8cb17c645610e5a85c738e09da77426f
Emile <emile.app@gmail.com>
2017-01-15 18:43:03 +0200

    moved from trigger to raycast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEditor;
 
 public class Player : MonoBehaviour {
 
-	private float gravitySpeed = -3.0f;
+	private float gravitySpeed = -2.0f;
 	public float walkSpeed = 2.0f;
 	private bool grounded = false;
 	public LayerMask groundLayer;
@@ -13,15 +14,64 @@ public class Player : MonoBehaviour {
 	public Transform isGrounded;
 	private float groundCheckRadius = 0.2f;
 
+	// ground raycast
+	private Bounds playerbounds;
+	private float rayLength;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		playerbounds = gameObject.GetComponent<BoxCollider>().bounds;
+		rayLength = playerbounds.size.y / 2 + 1.0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+
+//		float inputH = Input.GetAxisRaw ("Horizontal");
+//		float inputV = Input.GetAxisRaw ("Vertical");
+//
+//		float xMove = inputH;
+//		float zMove = inputV;
+//
+//		// gravity
+////		grounded = Physics.CheckSphere (isGrounded.position, groundCheckRadius, groundLayer);
+//		if (!grounded) {
+//			Debug.Log ("Should not be falling");
+//			zMove += gravitySpeed;
+//		} else {
+//			zMove = 0;
+//		}
+//
+//		Vector3 velocityDirection = new Vector3 (xMove, 0, zMove);
+//
+//		transform.Translate(velocityDirection * walkSpeed * Time.deltaTime);
+
+	}
+
+	void FixedUpdate ()
+	{
+//		float inputH = Input.GetAxisRaw ("Horizontal");
+//		float inputV = Input.GetAxisRaw ("Vertical");
+//
+//		float xMove = inputH;
+//		float zMove = inputV;
+//
+//		// gravity
+////		grounded = Physics.CheckSphere (isGrounded.position, groundCheckRadius, groundLayer);
+//		if (!grounded) {
+//			Debug.Log ("Should not be falling");
+//			zMove += gravitySpeed;
+//		} else {
+//			zMove = 0;
+//		}
+//
+//		Vector3 velocityDirection = new Vector3 (xMove, 0, zMove);
+//
+//		transform.Translate(velocityDirection * walkSpeed * Time.deltaTime);
+
+		// Raycast
 
 		float inputH = Input.GetAxisRaw ("Horizontal");
 		float inputV = Input.GetAxisRaw ("Vertical");
@@ -29,13 +79,25 @@ public class Player : MonoBehaviour {
 		float xMove = inputH;
 		float zMove = inputV;
 
+		Vector3 dwn = transform.TransformDirection (-Vector3.forward);
+
+		RaycastHit rayHit;
+		bool hit = Physics.Raycast (transform.position, dwn, rayLength, groundLayer, out rayHit);
+
+//		Debug.Log("Hit distance: " + hit.distance);
+
+		Debug.DrawRay (transform.position, dwn * rayLength, Color.green);
+		if (hit) {
+			print ("There is something below the object!");
+			grounded = false;
+		}
+
 		// gravity
-		grounded = Physics.CheckSphere (isGrounded.position, groundCheckRadius, groundLayer);
 		if (!grounded) {
 			Debug.Log ("Should not be falling");
 			zMove += gravitySpeed;
 		} else {
-			zMove = 0f;
+			zMove = 0;
 		}
 
 		Vector3 velocityDirection = new Vector3 (xMove, 0, zMove);
@@ -44,7 +106,15 @@ public class Player : MonoBehaviour {
 
 	}
 
-	void FixedUpdate(){
-
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.layer == 8) {
+			grounded = true;
+		}
+	}
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.layer == 8) {
+			grounded = false;
+		}
 	}
 }
